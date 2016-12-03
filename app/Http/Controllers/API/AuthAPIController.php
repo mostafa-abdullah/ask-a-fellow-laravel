6180eebe437ecd0a08f616f67fcc648725b3ca42
Hossam Ahmed <hossam.elfar95@gmail.com>
2016-12-03 12:53:19 +0300

    Now any use can be authenticated using ai end-point

--- app/Http/Controllers/API/AuthAPIController.php
+++ app/Http/Controllers/API/AuthAPIController.php
@@ -94,6 +94,46 @@ class AuthAPIController extends Controller
         }
 
     }
+
+    /**
+     * Login for a volunteer
+     * @param  Request $request: must contain email and password of the volunteer
+     * @return json response containing an error in case of invalid credentials or
+     * a server error or the token in case of valid credentials
+     */
+    public function login(Request $request)
+    {
+        // verify the credentials
+        $credentials = $request->only('email', 'password');
+
+        if(!Auth::attempt($credentials, false, false))
+            return response()->json(['error' => 'Invalid Credentials'], 401);
+
+        //create token
+        try
+        {
+            $user = User::where('email','=',$credentials['email'])->first();
+            $customClaims = ['type' => 'volunteer',
+                'id'  => $user->id,
+                'email' => $user->email];
+            $payload = JWTFactory::make($customClaims);
+            $token = JWTAuth::encode($payload);
+        }
+        catch (JWTException $e)
+        {
+            // something went wrong
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        // no errors, return the token
+        return Response::json(['token' => $token->get()]);
+    }
+
+
+
+
+
+
     /**
      * Logout for a User
      */
