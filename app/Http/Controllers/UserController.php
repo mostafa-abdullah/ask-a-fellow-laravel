97df69ac5f9c06b1dbc85567a98be488226713a7
Hossam Ahmed <hossam.elfar95@gmail.com>
2017-03-18 23:53:23 +0300

    Moving image hosting to cloudinary

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,13 +7,13 @@ use App\Http\Requests;
 use Auth;
 use App\Major;
 use Illuminate\Support\Facades\Session;
+use Cloudinary\Uploader;
 
 class UserController extends Controller
 {
-
     public function __construct()
     {
-        $this->middleware('auth',['only'=>[
+        $this->middleware('auth', ['only' => [
             'updateInfoPage'
         ]]);
     }
@@ -22,30 +21,27 @@ class UserController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        if(!$user)
+        if (!$user)
             return 'Ooops! User doesn\'t exit';
-        return view('user.questions',compact(['user']));
+        return view('user.questions', compact(['user']));
     }
-
 
     public function showProfileAnswers($id)
     {
         $user = User::find($id);
-        if(!$user)
+        if (!$user)
             return 'Ooops! User doesn\'t exit';
-        return view('user.answers',compact(['user']));
+        return view('user.answers', compact(['user']));
     }
-
 
     public function updateInfoPage()
     {
         $user = Auth::user();
-        if(!$user)
+        if (!$user)
             return 'Ooops! Not authorized';
         $majors = Major::all();
-        return view('user.update',compact(['user','majors']));
+        return view('user.update', compact(['user', 'majors']));
     }
-
 
     public function updateInfo(Request $request)
     {
@@ -55,45 +51,48 @@ class UserController extends Controller
             'major' => 'sometimes|numeric|exists:majors,id',
             'semester' => 'numeric|min:0|max:10',
             'profile_picture' => 'image|max:1000'
-
         ]);
-
-
         $user = Auth::user();
-
         $user->first_name = $request->first_name;
         $user->last_name = $request->last_name;
         $user->semester = $request->semester;
-        if($request->major)
+        if ($request->major)
             $user->major_id = $request->major;
         else
             $user->major_id = null;
         $user->bio = $request->bio;
-
-        if($request->file('profile_picture'))
-        {
-
-            //change profile picture
-            if($user->profile_picture)
-                unlink('../public'.Auth::user()->profile_picture);
+        if ($request->file('profile_picture')) {
+            \Cloudinary::config(array(
+                "cloud_name" => env("CLOUDINARY_NAME"),
+                "api_key" => env("CLOUDINARY_KEY"),
+                "api_secret" => env("CLOUDINARY_SECRET")
+            ));
+            if ($user->profile_picture) {
+                // delete previous profile picture
+                $this->delete_image($user->profile_picture);
+            }
+            // upload and set new picture
             $file = $request->file('profile_picture');
-            $photoname = uniqid();
-
-            $file_name = $photoname.'.'.$file->guessClientExtension();
-
-            //resize here
-            $file->move('../public/art/pp',$file_name);
-            $user->profile_picture = '/art/pp/' . $photoname . '.'.$file->guessClientExtension();
+            $image = Uploader::upload($file->getRealPath(), ["width" => 300, "height" => 300, "crop" => "limit"]);
+            $user->profile_picture = $image["url"];
         }
-
         $user->save();
-
-
-
-
-        Session::flash('updated','Info updated successfully!');
-        return redirect(url('/user/'.$user->id));
+        Session::flash('updated', 'Info updated successfully!');
+        return redirect(url('/user/' . $user->id));
     }
 
-
+    /**
+     * @param uri : the uri to the image to be deleted
+     */
+    private function delete_image($uri)
+    {
+        // extract the public id
+        $tmp = explode('/', $uri);
+        $public_id = end($tmp);
+        // remove the extension
+        $tmp = explode('.', $public_id);
+        $tmp = array_slice($tmp, 0, -1);
+        $public_id = implode(".", $tmp);
+        Uploader::destroy($public_id);
+    }
 }
